baa0673e1f31fde53b5c4f185f2eb0cc80d03728
skyzyy <122117174+skyzxu-boop@users.noreply.github.com>
2024-12-10 10:00:51 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const fs = require('node:fs');
 
 const config = {
-    owner: ["6285165556936", "6285215909004"],
+    owner: ["62881027613221"],
     name: "- nekoBot - Simple WhatsApp bot",
     sessions: "sessions",
     sticker: {
