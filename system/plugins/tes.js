407afaa62a0bb550c6c8f088bf27dd474f1bad9b
Axel <axellsuppport@gmail.com>
2024-12-16 14:18:17 +0700

    Delete tes.js

--- system/plugins/tes.js
+++
@@ -1,5 +0,0 @@
-module.exports  = {
-    async events(m, { sock, Func }) {
-      if (m.isGroup && Func.isUrl(m.body)) return m.reply(`> Terdeteksi @${m.sender.split("@")[0]} mengirim link`);
-  }
-}
